b297f0b9ca5e72e0f1711973b66a96eaeea82095
m2583945 <60531328+m2583945@users.noreply.github.com>
2026-05-24 20:51:08 -0500

    Yarn spinner dl + dress code minigame setup
    added yarn spinner package, initial dialogue/cutscene script, dress code minigame script, and dress code prototype assets

---
+++ Assets/Scripts/DialogueHandler.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Yarn.Unity
+{
+    public class DialogueHandler : MonoBehaviour
+    {
+        bool lockVisited;
+        public DialogueRunner dr;
+        public GameObject dialogueSystem;
+        public List<GameObject> disabledDuringDialogue;
+        private bool isDialogueActive;
+        public bool allowClicks = true;
+        public Animator spriteAnimator;
+        public Image portrait;
+        public Image portrait2;
+        public Sprite[] vex;
+        public Sprite[] robogirl;
+        public GameObject characterName;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+            characterName.gameObject.SetActive(true);
+            //vs = GameObject.FindFirstObjectByType<InMemoryVariableStorage>();
+            dr.AddCommandHandler<string>("setVex", setVex);
+            dr.AddCommandHandler<string>("setRobo", setRobo);
+            runNode("Cutscene1");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void setRobo(string spriteName)
+        {
+            characterName.GetComponent<TMPro.TextMeshProUGUI>().text = "EL1-5E";
+            characterName.gameObject.SetActive(true);
+            if(portrait.gameObject.active == true)
+            {
+                //portrait.gameObject.SetActive(false);
+                portrait2.gameObject.SetActive(true);
+            }
+            if(spriteName == "robo")
+            {
+                portrait2.gameObject.GetComponent<Image>().sprite = robogirl[0];
+            }
+            
+            //portrait2.gameObject.GetComponent<Image>().SetNativeSize();
+        }
+
+        void setVex(string spriteName)
+        {
+            characterName.GetComponent<TMPro.TextMeshProUGUI>().text = "Vex";
+            characterName.gameObject.SetActive(true);
+            if (portrait2.gameObject.active == true)
+            {
+                //portrait2.gameObject.SetActive(false);
+                portrait.gameObject.SetActive(true);
+            }
+            portrait.gameObject.GetComponent<Image>().sprite = vex[0];
+            if (spriteName == "vexSad")
+            {
+                portrait.gameObject.GetComponent<Image>().sprite = vex[1];
+            }
+            //portrait.gameObject.GetComponent<Image>().SetNativeSize();
+        }
+
+
+
+
+        void setEveryone(string everyone)
+        {
+            characterName.GetComponent<TMPro.TextMeshProUGUI>().text = "Everyone";
+            portrait.gameObject.GetComponent<Image>().enabled = false;
+        }
+        
+        public void runNode(string nodeName)
+        {
+            StartDialogue(nodeName);
+        }
+
+        public void cutscene1()
+        {
+            setVex("vex");
+            StartDialogue("Cutscene1");
+        }
+
+        private void StartDialogue(string convo)
+        {
+            dialogueSystem.gameObject.SetActive(true);
+            portrait.gameObject.GetComponent<Image>().enabled = true;
+            isDialogueActive = true;
+            dr.StartDialogue(convo);
+        }
+        
+        public void EndDialogue(string end)
+        {
+            dialogueSystem.gameObject.SetActive(false);
+            portrait.gameObject.GetComponent<Image>().enabled = false;
+            isDialogueActive = false;
+        }
+        
+        public bool isActive()
+        {
+            return isDialogueActive;
+        }
+       
+        public void allowObjectClicks()
+        {
+            print("allow");
+            allowClicks = true;
+        }
+        public void disableClicks()
+        {
+            allowClicks = false;
+        }
+
+    }
+}
---
+++ Library/PackageCache/dev.yarnspinner.unity@cc0f3c283413/Runtime/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿/*
+Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("3.1.4.0")]
+[assembly: AssemblyFileVersion("3.1.4.0")]
+[assembly: AssemblyInformationalVersion("3.1.4.Branch.hotfix/textanim-build-errors.Sha.c2b119c5eda7fdd3cd0b13a689f95d54d456fb69")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Tests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Tests.Editor")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
+
